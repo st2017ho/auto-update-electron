1592576dcbdcb12ad25e05ac4c1f93b4a64f9c11
st2017ho <st_2017_ho@yahoo.com>
2023-04-30 10:50:59 +0800

    commit test done

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
-log.transports.file.resolvePath = () => path.join(__dirname, "logs/main.log");
-log.info("Hello, log");
-log.warn("Some problem appears");
+log.transports.file.resolvePath = () =>
+  path.join(app.getPath("logs"), "main.log");
+log.info("Application version =" + app.getVersion());
 
 let win;
 function createWindow() {
@@ -17,18 +18,27 @@ app.on("ready", () => {
   autoUpdater.checkForUpdatesAndNotify();
 });
 
-autoUpdater.on("update-available", () => {
-  log.info("update-available");
-});
-
 autoUpdater.on("checking-for-update", () => {
-  log.info("checking-for-update");
+  log.info("Checking for update...");
 });
 
-autoUpdater.on("download-progress", () => {
-  log.info("download-progress");
+autoUpdater.on("update-available", (info) => {
+  log.info("Update available");
+});
+
+autoUpdater.on("update-not-available", (info) => {
+  log.info("Update not available");
+});
+
+autoUpdater.on("error", (error) => {
+  log.info("Error in auto-updater. " + error);
+});
+
+autoUpdater.on("download-progress", (progressTrack) => {
+  log.info("\n\ndownload-progress");
+  log.info(progressTrack);
 });
 
 autoUpdater.on("update-downloaded", () => {
-  log.info("update-downloaded");
+  log.info("Update downloaded");
 });
